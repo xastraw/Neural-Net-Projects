a5caefde5a8f88a85dd7008639af6fa30d732ed1
Xander Straw <113134484+xastraw@users.noreply.github.com>
2025-12-10 22:50:40 -0500

    best net yet

--- slurm-train.sh
+++ slurm-train.sh
@@ -7,8 +7,6 @@
 
 
 source /home/jhub/jhub-venv/bin/activate
-#source ~/miniconda3/etc/profile.d/conda.sh
-#conda activate neural-net-minst
 
 
 export LD_LIBRARY_PATH=/home/jhub/jhub-venv/lib/python3.10/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH
